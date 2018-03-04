e469d5f617e8134663066d0e41c8ae035169eb63
Karle Sleith <karlesleith@gmail.com>
2018-03-04 19:18:21 +0000

    Cosmetic Changes to UI

---
+++ 1337/1337/Assets/Scripts/MapBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapBuilder : MonoBehaviour {
+
+	public void PlayMap()
+    {
+        Debug.Log("Debug: Playing user generated level");
+    }
+    public void SaveMap()
+    {
+        Debug.Log("Debug: Saving the User Level");
+    }
+    public void QuitToMain()
+    {
+        Debug.Log("Debug: Returning to MainMenu");
+        SceneManager.LoadScene(0);
+    }
+   
+}
